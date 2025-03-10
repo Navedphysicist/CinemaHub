81e8b30146684aedd2cbad84f92d7d787e31c23f
Naved Khan <navedphsysicist@gmail.com>
2025-03-10 14:04:54 +0530

    Update to the postgres

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "sqlite:///./cinemaHub.db"
+# DATABASE_URL = "sqlite:///./cinemaHub.db"
+DATABASE_URL = "postgresql://postgres:Cinema@8755@db.siimrhwdwfplsconkmnd.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
